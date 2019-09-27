8154c95bd7d79d842fe55d65755bb875905c73a9
Henrique Broering Rohden <hrohden@gmail.com>
2019-09-27 13:55:50 -0300

    Add Spring Boot project files

---
+++ src/main/java/com/henriquerohden/udacitycapstone/UdacitycapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.henriquerohden.udacitycapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdacitycapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdacitycapstoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henriquerohden/udacitycapstone/UdacitycapstoneApplicationTests.java
@@ -0,0 +1,16 @@
+package com.henriquerohden.udacitycapstone;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UdacitycapstoneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
